32bb46514d38e0688c82ab9845247090435ad44d
Navya Thakur <Navya.thakur@zeuslearning.com>
2022-12-05 18:11:03 +0530

    query variables

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 import {ApolloServer, gql} from 'apollo-server'
 import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
+import {users, quotes} from './fakedb.js'
 
 const typeDefs = gql `
-   type Query{
-       greet:String
-   }
+    type Query{
+        users:[User]
+        user(id:ID!): User
+        quotes:[Quote]
+        iquote(by:ID!):[Quote]
+    }
+    type User{
+        id:ID!
+        firstName:String
+        lastName:String
+        email:String
+        quotes:[Quote]
+    }
+    type Quote{
+        name:String
+        by:ID
+    }
 `;
 
 const resolvers = {
     Query: {
-        greet: () => {
-            return "Hello world"
-        }
+        users: () => users,
+        user: (_, {id}) => users.find(user => user.id == id),
+        quotes: () => quotes,
+        iquote: (_, {by}) => quotes.filter((quote) => quote.by == by)
+    },
+    User: {
+        quotes: (ur) => quotes.filter(quote => quote.by == ur.id)
     }
 }
 
